b28a39fda7cd7c6176f1ca0909b18c63c40f71d5
milanh34 <milanh344@gmail.com>
2024-02-17 22:58:26 +0530

    Backend links changed

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://quora-clone-aug9.onrender.com',
+  baseURL: 'https://quora-clone-aug9.onrender.com',
 }); 
